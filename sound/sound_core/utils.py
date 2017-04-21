a1d434b61e553708572f0454a155a2fb26aff64b
jguardado39 <jguardado39@gmail.com>
2017-04-20 17:59:01 -0700

    fixed

--- sound/sound_core/utils.py
+++ sound/sound_core/utils.py
@@ -22,35 +22,35 @@ class FileIndexer:
 			for k, v in id3.items():
 				tags[k] = v[0].lower()
 
-			# if file name does not contain artist/title 
+			""" if file name does not contain artist/title """
 
 			if tags["artist"] is None or tags["title"] is None:
 				print "Artist or title not set in " + \
 					filename + " - skipping file"
 				return
 
-			# if file name contains artist
+			""" if file name contains artist """
 
 			if tags["artist"] is not None:
 				tags["artist"], created = Album.objects.get_or_create(
 					Name = tags["artist"]
 					)
 
-			# if file name contains album AND artist
+			""" if file name contains album AND artist """
 
 			if tags["album"] is not None and tags["artist"] is not None:
 				tags["album"], created = Album.objects.get_or_create(
 					Title = tags["album"]
 					)
 
-			# if file name DOES NOT contain genre
+			""" if file name DOES NOT contain genre """
 
 			if tags["genre"] is not None:
 				tags["genre"], created = Genre.objects.get_or_create(
 					Name = tags["genre"]
 					)
 
-			# if file name DOES NOT contian date
+			""" if file name DOES NOT contian date """
 
 			if tags["date"] is not None:
 				try:
